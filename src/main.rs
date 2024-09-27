44c25d58b4b5e868acfe8d92b3b14c401f846ef8
Flemmli97 <Flemmli97@users.noreply.github.com>
2024-09-27 16:10:11 +0200

    fix non root path url

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,14 @@ use crate::cache::Cache;
 use crate::db::Database;
 use crate::env::BASE_PATH;
 use crate::errors::Error;
-use axum::extract::{DefaultBodyLimit, FromRef};
-use axum::Router;
+use axum::extract::{DefaultBodyLimit, FromRef, Request};
+use axum::{Router, ServiceExt};
 use axum_extra::extract::cookie::Key;
+use tower_http::normalize_path::NormalizePathLayer;
 use std::process::ExitCode;
 use std::time::Duration;
 use tokio::net::TcpListener;
-use tower::ServiceBuilder;
+use tower::{Layer, ServiceBuilder};
 use tower_http::compression::CompressionLayer;
 use tower_http::timeout::TimeoutLayer;
 use tower_http::trace::TraceLayer;
@@ -107,9 +108,10 @@ async fn start() -> Result<(), Box<dyn std::error::Error>> {
     tracing::debug!("restricting maximum body size to {max_body_size} bytes");
 
     let service = make_app(max_body_size, timeout).with_state(state);
+    let app = NormalizePathLayer::trim_trailing_slash().layer(service);
     let listener = TcpListener::bind(&addr).await?;
 
-    axum::serve(listener, service)
+    axum::serve(listener, ServiceExt::<Request>::into_make_service(app))
         .with_graceful_shutdown(shutdown_signal())
         .await?;
 
